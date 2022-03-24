24f44929be2f9d1afac0ca7169ea0f33c90a469a
Thibeau Verhoeven <thibeau@verhoeven.ws>
2022-03-24 15:23:29 +0100

    f

---
+++ TennisApp/TennisApp/ViewModels/NavigationPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TennisApp.ViewModels
+{
+    public class NavigationPageViewModel : ViewModelBase
+    {
+        public NavigationPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            
+        }
+    }
+}
---
+++ TennisApp/TennisApp/ViewModels/SpelersLijstViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TennisApp.ViewModels
+{
+    public class SpelersLijstViewModel : ViewModelBase
+    {
+        public SpelersLijstViewModel(INavigationService navigationService) 
+            : base(navigationService)
+        {
+            
+        }
+    }
+}
