d99969b66d8760c7346ed2c1eedd1f9be239bba1
Thibeau Verhoeven <thibeau@verhoeven.ws>
2022-03-31 14:51:04 +0200

    f

--- TennisApp/TennisApp/ViewModels/NavigationPageViewModel.cs
+++ TennisApp/TennisApp/ViewModels/NavigationPageViewModel.cs
@@ -15,7 +15,7 @@ namespace TennisApp.ViewModels
         {
             Title = "NavigationEventArgs Page";
 
-            SpelersLijstPageCommand = new DelegateCommand(async () => );
+            SpelersLijstPageCommand = new DelegateCommand(async () =>  );
         }
 
         public DelegateCommand SpelersLijstPageCommand { get; }
